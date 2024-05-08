cc997d55d61d8b0d5ad9f7e7979295aea593d36b
ahmadharissa <ahmadharissa25@gmail.com>
2024-05-08 12:47:47 +0300

    Tabadol

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -8,6 +8,7 @@ import TVC from "../../Assets/Projects/TVC.png";
 import Kawwen from "../../Assets/Projects/Kawwen.png";
 import Mehnati from "../../Assets/Projects/Mehnati.png";
 import Portfolio from "../../Assets/Projects/Portfolio.png";
+import tabadol from "../../Assets/Projects/tabadol.png";
 
 function Projects() {
   return (
@@ -26,7 +27,7 @@ function Projects() {
               isBlog={false}
               title="TVC"
               description="We at The Volunteer Circle believe it is time to take charge by creating a platform for skill-sharing across all areas of Lebanon, we match enthusiastic volunteers, community services, experience seekers, and multi-talented individuals with the cause that needs them the most."
-              demoLink="https://potal.thevolunteercircle.hostbotics.net/"
+              demoLink="https://portal.thevolunteercircle.com/"
             />
           </Col>
           <Col md={4} className="project-card">
@@ -45,6 +46,15 @@ function Projects() {
               title="Mehnati"
               description="Mehnati is an online platform dedicated to raising awareness about vocational and technical education in Lebanon. This website introduces academic school students to the aspects of vocational education and guides them through the process of career development, by assessing their skills and helping them explore their career path, develop their abilities, as well as plan for their future."
               demoLink="https://jobs.mehnati.hostbotics.net/"
+            />
+          </Col>
+          <Col md={4} className="project-card">
+            <ProjectCard
+              imgPath={tabadol}
+              isBlog={false}
+              title="Tabadol"
+              description="Tabadol is an online platform."
+              demoLink="https://frontend.tabadol.hostbotics.net/"
             />
           </Col>
           <Col md={4} className="project-card">
